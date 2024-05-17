f56ca9aa49746b87efb8b4851a7d819c3abdb2fc
basel2053 <baselsalah2053@gmail.com>
2024-05-18 02:54:00 +0300

    feat: add pgx and intialize db pool

--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"basel2053/ps-board/api"
+	"basel2053/ps-board/db"
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -10,9 +12,16 @@ import (
 )
 
 func main() {
+	dbpool, err := db.NewPG(context.Background())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer dbpool.Close()
+
 	listenAddr := flag.String("listenaddr", os.Getenv("PORT"), "Server port")
 	http.HandleFunc("/", api.RootHandler)
 	fmt.Printf("Server is up on running on http://localhost%s\n", *listenAddr)
-	err := http.ListenAndServe(*listenAddr, nil)
+	err = http.ListenAndServe(*listenAddr, nil)
 	log.Fatal(err)
 }
